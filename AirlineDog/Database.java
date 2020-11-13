60b8ef3d87f210d5bf0338732fd81b39c1586081
AirlineDog <t8190042@aueb.gr>
2020-11-13 15:11:20 +0200

    database

--- AirlineDog/Database.java
+++ AirlineDog/Database.java
@@ -33,34 +33,11 @@ public class Database {
 
 	}
 
-	private static void createConnection() {
-		try {
-			Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
-            //Get a connection
-            conn = DriverManager.getConnection(dbURL); 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-	}
-	
-	 private static void shutdown() {
-	        try {
-	            if (stmt != null) {
-	                stmt.close();
-	            }
-	            if (conn != null) {
-	                DriverManager.getConnection(dbURL + ";shutdown=true");
-	                conn.close();
-	            }           
-	        }catch (SQLException sqlExcept) {
-	            sqlExcept.printStackTrace();
-	        }
 
-	    }
-	 private static void createTable() {
+	 private static void createTable(String tableName) {
 		 try {
 			 stmt =conn.createStatement();
-			 stmt.execute("CREATE TABLE Users (id INT NOT NULL,Name VARCHAR(255),cityName VARCHAR(255),PRIMARY KEY ( id) )");
+			 stmt.execute("CREATE TABLE " + tableName + " (id INT NOT NULL,Name VARCHAR(255),cityName VARCHAR(255),PRIMARY KEY ( id) )");
 			 stmt.close();
 		 }catch(Exception e) {
 			 e.printStackTrace();
@@ -106,4 +83,28 @@ public class Database {
 	            sqlExcept.printStackTrace();
 	        }
 	    }
+	 
+		private static void createConnection() {
+			try {
+				Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+	            //Get a connection
+	            conn = DriverManager.getConnection(dbURL); 
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+		}
+		
+		 private static void shutdown() {
+		        try {
+		            if (stmt != null) {
+		                stmt.close();
+		            }
+		            if (conn != null) {
+		                DriverManager.getConnection(dbURL + ";shutdown=true");
+		                conn.close();
+		            }           
+		        }catch (SQLException sqlExcept) {
+		            sqlExcept.printStackTrace();
+		        }
+		    }
 }
