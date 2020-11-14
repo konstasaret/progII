b9818bde67db74aed2a4d8e750983c940dfa74dd
AirlineDog <t8190042@aueb.gr>
2020-11-14 12:01:24 +0200

    server

--- AirlineDog/MultithreadedSocketServer.java
+++ AirlineDog/MultithreadedSocketServer.java
@@ -8,22 +8,17 @@ public class MultithreadedSocketServer {
             ServerSocket server=new ServerSocket(8888);
             Profile profile = new Profile();
 
-            profile.setID(ReadWrite.read("ID.txt"));
-            profile.setUser_name(ReadWrite.read("User.txt"));
-            profile.setPassword(ReadWrite.read("Pass.txt"));
-            profile.setAddress(ReadWrite.read("Address.txt"));
-
             int counter=0;
             System.out.println("Server Started ....");
             while(true){
                 counter++;
                 Socket serverClient=server.accept();  //server accept the client connection request
-                System.out.println(" >> " + "Client No:" + counter + " started!");
+                System.out.println(">> " + "Client No:" + counter + " started!");
                 ServerClientThread sct = new ServerClientThread(serverClient,counter); //send  the request to a separate thread
                 sct.start();
             }
         }catch(Exception e){
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
