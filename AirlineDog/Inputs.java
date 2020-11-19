cafbacc25ebd5ea6e21c8dde944a9a243c5993c9
AirlineDog <t8190042@aueb.gr>
2020-11-19 14:15:52 +0200

    inputs resolved

--- AirlineDog/Inputs.java
+++ AirlineDog/Inputs.java
@@ -4,13 +4,12 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Inputs {
-	
-	private static Scanner in = new Scanner(System.in);
-	
-	
+
 	/**Handles user input errors and returns an integer in the range of
 	 *  lower_boundary and upper_boundary including the limits*/
 	public static int rangeInt(int lower_boundary, int upper_boundary) {
+		Scanner in = new Scanner(System.in);
+		
 		int inp = -1;
 		try {
 			inp = in.nextInt();
@@ -25,9 +24,11 @@ public class Inputs {
 		System.out.println("Επιλέξατε : " + inp);
 			return inp;	
 	}
+	
+	
 	/** Returns user String input*/
 	public static String stringScanner () {
-		
+		Scanner in = new Scanner(System.in);
 		return in.nextLine();
 	}
 	
