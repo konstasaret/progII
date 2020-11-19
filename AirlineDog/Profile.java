1239e80c7dc2f4a4d54501e8c3ec2b6e4b774eca
AirlineDog <t8190042@aueb.gr>
2020-11-19 13:17:55 +0200

    Inputs configuration

--- AirlineDog/Profile.java
+++ AirlineDog/Profile.java
@@ -1,30 +1,27 @@
 package AirlineDog;
 
-import java.util.Scanner;
-
 public class Profile {
 	
 
 	/**Creates new user */
 	public static void newEntry() {
 		Database.createConnection();		
-		Scanner in = new Scanner(System.in);
 				
 				System.out.println("Παρακαλώ εισάγετε Όνομα Χρήστη");
-				String user_name=in.nextLine();
+				String user_name = Inputs.stringScanner();
 				while (Database.usernameCheck(user_name)) {
 					System.out.println("Το Όνομα Χρήστη χρησημοποιείται");
 					System.out.println("Παρακαλώ διαλέξτε διαφορετικό Όνομα Χρήστη");
-					user_name = in.nextLine();
+					user_name = Inputs.stringScanner();
 				}
 				
 
 				String pass, pass2;
 				do {
 					System.out.println("Παρακαλώ εισάγετε Κωδικό Χρήστη");
-					pass = in.nextLine();
+					pass = Inputs.stringScanner();
 					System.out.println("Παρακαλώ επιβεβαιώστε τον Κωδικό Χρήστη σας");
-					pass2 = in.nextLine();
+					pass2 = Inputs.stringScanner();
 					if(!pass.equals(pass2)) {
 						System.out.println("Οι Κωδικοί σας δεν ταιριάζουν \nΔοκιμάστε Ξανά");
 					}
@@ -44,20 +41,18 @@ public class Profile {
 		/**Authenticates user's credentials*/
 		public static void authenticate() {
 			Database.createConnection();		
-
-			Scanner in = new Scanner(System.in);
 			
 			System.out.println("Παρακαλώ εισάγετε το Όνομα Χρήστη σας");
-			String name = in.nextLine();
+			String name = Inputs.stringScanner();
 			
 			String userPass = Database.findUserName(name);
 			while (userPass.equals("-1")) {
 				System.out.println("Αποτυχία Συνδεσης\nΤο Όνομα Χρήστη δεν υπάρχει\nΠαρακαλώ προσπαθήστε ξανά :");
-				name = in.nextLine();
+				name = Inputs.stringScanner();
 				userPass = Database.findUserName(name);
 			}
 			System.out.println("Παρακαλώ εισάγετε τον Κωδικό σας");
-			String pass = in.nextLine();
+			String pass = Inputs.stringScanner();
 			
 				
 			if (userPass.equals(pass)) {
@@ -66,7 +61,7 @@ public class Profile {
 				while(!userPass.equals(pass)) {
 				System.out.println("Αποτυχία Συνδεσης\nΤο Όνομα Χρήστη και ο Κωδικός δεν ταιριάζουν\nΔοκιμάστε ξανά");
 				System.out.println("Παρακαλώ εισάγετε τον Κωδικό σας");
-				pass = in.nextLine();
+				pass = Inputs.stringScanner();
 				}
 			}
 			Database.shutdownConnection();
