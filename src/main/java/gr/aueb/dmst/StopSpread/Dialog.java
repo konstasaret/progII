e113377c273a50718426c87d10bab74285aad1b3
= <t8190130@aueb.gr>
2020-12-05 17:38:51 +0200

    GIA na bgazei se morfi box ta exception an ueloume mporoume na to xrissimopoisoume

---
+++ src/main/java/gr/aueb/dmst/StopSpread/Dialog.java
@@ -0,0 +1,17 @@
+//an ueloume na to xrisimopoiisoume oste stis ejereseis na to bgazei me ti morfi box
+package gr.aueb.dmst.StopSpread;
+
+/**dialog with errors*/
+/** Printing multiple lines in a dialog box.*/
+
+/**import class JOptionPane*/
+import javax.swing.JOptionPane;
+
+public class Dialog
+{
+	public static void main(String [] args)
+	{
+		/**dispaly a dialog with the message*/
+		JOptionPane.showMessageDialog(null,"Error");
+	} //ending of main
+} //ending of the class Dialog
