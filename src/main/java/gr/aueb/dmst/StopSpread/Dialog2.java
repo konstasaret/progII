b91f73443dc5d611bbe7e59cc86b6d9abe33dc38
vickyviolintzi <t8190013@aueb.gr>
2020-12-06 11:59:02 +0200

    άλλαξα τη σειρά javadoc

--- src/main/java/gr/aueb/dmst/StopSpread/Dialog2.java
+++ src/main/java/gr/aueb/dmst/StopSpread/Dialog2.java
@@ -1,21 +1,20 @@
 package gr.aueb.dmst.StopSpread;
 
-/**dialog input with a dialog box */
-
 /**import class JOptionPane*/
 import javax.swing.JOptionPane;
 
+/**dialog input with a dialog box */
 public class Dialog2
 {
 	public  void  Dialog2Boxes()
 	{
 		/**promt user to enter the name*/
 		String name =
-			JOptionPane.showInputDialog("Insert your code");
+			JOptionPane.showInputDialog("Insert your password:");
 
 			/**create the message*/
 			String message =
-				String.format(" Welcome to our app");
+				String.format(" Welcome to our app!");
 
 			/**display the message user's code */
 			JOptionPane.showMessageDialog(null,message);
