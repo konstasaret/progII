16e36228153868ff3c437c42af5b97eccda05fd8
vickyviolintzi <t8190013@aueb.gr>
2020-12-06 12:01:18 +0200

    Μορφοποίηση

--- src/main/java/gr/aueb/dmst/StopSpread/Dialog.java
+++ src/main/java/gr/aueb/dmst/StopSpread/Dialog.java
@@ -1,16 +1,13 @@
 //an ueloume na to xrisimopoiisoume oste stis ejereseis na to bgazei me ti morfi box
 package gr.aueb.dmst.StopSpread;
 
-/**dialog with errors*/
-/** Printing multiple lines in a dialog box.*/
-
 /**import class JOptionPane*/
 import javax.swing.JOptionPane;
 
-public class Dialog
-{
-	public void DialogBox()
-	{
+/**dialog with errors*/
+/** Printing multiple lines in a dialog box.*/
+public class Dialog {
+	public void DialogBox() {
 		/**dispaly a dialog with the message*/
 		JOptionPane.showMessageDialog(null,"Error");
 	} //ending of Dialog
