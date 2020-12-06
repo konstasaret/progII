37b9612b7b4c183efb222aece3dd2fd50c814510
= <t8190130@aueb.gr>
2020-12-06 23:48:01 +0200

    diorthosi onomatos app kai prosthiki javadoc

--- src/main/java/gr/aueb/dmst/StopSpread/Frame1.java
+++ src/main/java/gr/aueb/dmst/StopSpread/Frame1.java
@@ -1,6 +1,9 @@
 /**it appears blue background with an icon and the name of our app up -left*/
 package gr.aueb.dmst.StopSpread;
 
+/**
+ * author Evaggelia
+ */
 import java.awt.Color;
 
 import javax.swing.JFrame;
@@ -12,7 +15,7 @@ public class Frame1{
 		//JFrame = a GUI window to add components to
 
 		JFrame frame = new JFrame(); //creates a frame
-		frame.setTitle("Welcome to CovidSpead App");
+		frame.setTitle("Welcome to StopCovidSpread App");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out of application//hide
 		frame.setResizable(false); //prevent frame from being reasized
 		frame.setSize(5000,5000); //sets the x-diamesion , and y-diamension of frame
