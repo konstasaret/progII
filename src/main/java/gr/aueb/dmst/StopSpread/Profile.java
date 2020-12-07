61d234d176a4ba73e0984dbd71ab092c03e1b5f7
AirlineDog <t8190042@aueb.gr>
2020-12-07 21:01:33 +0200

    date input check

--- src/main/java/gr/aueb/dmst/StopSpread/Profile.java
+++ src/main/java/gr/aueb/dmst/StopSpread/Profile.java
@@ -138,7 +138,7 @@ public class Profile {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Inserts new user location in the database 
 	 * 
@@ -172,9 +172,12 @@ public class Profile {
             dep_time = Inputs.rangeInt(1,24);
         }
         
-        System.out.println("Εισάγετε την ημερομηνία της επίσεψής σας (ΥΥΥΥ-MM-DD) :");
+        System.out.println("Εισάγετε την ημερομηνία της επίσεψής σας (ΕΕΕΕ-ΜΜ-ΗΗ) :");
         String date = Inputs.stringScanner();
-        
+        while (!date.matches("^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$")) {
+        	System.err.println("Παρακαλώ εισάγεται την ημερομηνία στην εξής μορφή (ΕΕΕΕ-ΜΜ-ΗΗ) :");
+        	date = Inputs.stringScanner();
+        }
         
         try {
 	        //server-client messages
