ffb47349959ef69eac90edb19e475f5f62093bf3
vickyviolintzi <t8190013@aueb.gr>
2020-12-09 18:58:01 +0200

    Μορφοποίηση κώδικα

--- src/main/java/gr/aueb/dmst/StopSpread/MainEvaluation.java
+++ src/main/java/gr/aueb/dmst/StopSpread/MainEvaluation.java
@@ -2,7 +2,6 @@
 //na bgei i main
 
 package gr.aueb.dmst.StopSpread;
-
 import java.util.Scanner;
 
 /**author
@@ -11,9 +10,7 @@ import java.util.Scanner;
  * and offers the user the choices of both 
  * evaluate out up and write comments
  */
-
 public class MainEvaluation { //begginig of class
-	
 	public static void main(String [] args){
 		/**declaration of Scanner*/
 		Scanner sc = new Scanner(System.in);
@@ -30,36 +27,34 @@ public class MainEvaluation { //begginig of class
 		InsertingEvaluation objectReason = new InsertingEvaluation();
 		InsertingEvaluation objectSxolia = new InsertingEvaluation();
 
-		while(flag3){//beggining of loop
-		/**Appears the Evaluation Menu*/
-		objectMenouEvaluation.printMenu();
-		System.out.printf(" %s \n", "Παρακαλώ εισάγετε την επιλογή σας : ");
-
-		userchoice = sc.nextInt();
-
-		switch(userchoice){ //beggining of switch
-		case 1:
-		objectInsertingEvaluation.printEvaluation();
-		/**the user calling this class via the objectMax it shows witch critic dominates-kaki,metria
-		 * ,kali,poli kali app or  combinations of them */
-		objectMax.printDomination(); 
-		/**the user calling this class via the objectReason inerts their sxolia about our app*/
-		objectReason.reasonOfEvaluation(); 
-		break;
-		case 2:
-		/**the user choosing this alternative can evaluate our app
-		 * from 1(0ne) to 4(four) 1 = bad , 2 = moderate ,3 = good , 4 = very good */
-		objectInsertingEvaluation.insertEvaluation();
-		break;
-		case 3:
-		/**the user choosing this alternative can write their critics in order to have 
-		 * feedbach and can improve our app*/
-			objectSxolia.reasonOfEvaluation();
-		break;
-		case 4 :
-		flag3 = false;
-
-		} //ending of switch
+		while(flag3) {//beggining of loop
+			/**Appears the Evaluation Menu*/
+			objectMenouEvaluation.printMenu();
+			System.out.printf(" %s \n", "Παρακαλώ εισάγετε την επιλογή σας : ");
+			userchoice = sc.nextInt();
+			switch(userchoice){ //beggining of switch
+			case 1:
+				objectInsertingEvaluation.printEvaluation();
+				/**the user calling this class via the objectMax it shows witch critic dominates-kaki,metria
+				 * ,kali,poli kali app or  combinations of them */
+				objectMax.printDomination(); 
+				/**the user calling this class via the objectReason inerts their sxolia about our app*/
+				objectReason.reasonOfEvaluation(); 
+				break;
+			case 2:
+				/**the user choosing this alternative can evaluate our app
+				 * from 1(0ne) to 4(four) 1 = bad , 2 = moderate ,3 = good , 4 = very good */
+				InsertingEvaluation.insertEvaluation();
+				break;
+			case 3:
+				/**the user choosing this alternative can write their critics in order to have 
+				 * feedbach and can improve our app*/
+					objectSxolia.reasonOfEvaluation();
+				break;
+			case 4 :
+				flag3 = false;
+			} //ending of switch
 		} //ending of loop
+		sc.close();
 	 } //ending of main
-	} //ending of class MainEvaluation
+} //ending of class MainEvaluation
