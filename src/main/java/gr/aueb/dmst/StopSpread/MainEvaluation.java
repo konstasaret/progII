3b9ab096eb85d96637628fba526d2e4557550987
Chrysoula7 <xapriv@gmail.com>
2020-12-09 19:13:36 +0200

    fixed errors and changed main

--- src/main/java/gr/aueb/dmst/StopSpread/MainEvaluation.java
+++ src/main/java/gr/aueb/dmst/StopSpread/MainEvaluation.java
@@ -1,41 +1,32 @@
-//na prosueso ejereseis
-//na bgei i main
-
 package gr.aueb.dmst.StopSpread;
-import java.util.Scanner;
-
 /**author
- * Evaggelia
+ * Evaggelia, Vicky, Chrysoula
  * this class appears our app evaluation
  * and offers the user the choices of both 
  * evaluate out up and write comments
  */
-public class MainEvaluation { //begginig of class
-	public static void main(String [] args){
-		/**declaration of Scanner*/
-		Scanner sc = new Scanner(System.in);
+public class MainEvaluation { //beginning of class
+	public static void mainEval(){		
 		/**declaration of the variable stores user's choice*/
 		int userchoice = 0;
 		boolean flag3 = true;
-		
 		/**calling of MenouEvaluation*/
 		Menus objectMenouEvaluation = new Menus();
 		/*creation of Objects*/
-		InsertingEvaluation objectAlreadyEvaluation = new InsertingEvaluation();
+		InsertingEvaluation objectAlreadyEvaluation = new InsertingEvaluation(); //Ευαγγελία δε χρησιμοποιείται
 		InsertingEvaluation objectInsertingEvaluation = new InsertingEvaluation();
 		InsertingEvaluation objectMax = new InsertingEvaluation();
 		InsertingEvaluation objectReason = new InsertingEvaluation();
 		InsertingEvaluation objectSxolia = new InsertingEvaluation();
-
-		while(flag3) {//beggining of loop
+		while(flag3) {//beginning of loop
 			/**Appears the Evaluation Menu*/
 			objectMenouEvaluation.printMenu();
-			System.out.printf(" %s \n", "Παρακαλώ εισάγετε την επιλογή σας : ");
-			userchoice = sc.nextInt();
-			switch(userchoice){ //beggining of switch
+			System.out.printf(" %s \n", "Παρακαλώ εισάγετε την επιλογή σας: ");
+			userchoice = Inputs.rangeInt(1, 4);
+			switch (userchoice) { //beginning of switch
 			case 1:
 				objectInsertingEvaluation.printEvaluation();
-				/**the user calling this class via the objectMax it shows witch critic dominates-kaki,metria
+				/**the user calling this class via the objectMax it shows which critic dominates-kaki,metria
 				 * ,kali,poli kali app or  combinations of them */
 				objectMax.printDomination(); 
 				/**the user calling this class via the objectReason inerts their sxolia about our app*/
@@ -49,12 +40,11 @@ public class MainEvaluation { //begginig of class
 			case 3:
 				/**the user choosing this alternative can write their critics in order to have 
 				 * feedbach and can improve our app*/
-					objectSxolia.reasonOfEvaluation();
+				objectSxolia.reasonOfEvaluation();
 				break;
 			case 4 :
 				flag3 = false;
 			} //ending of switch
 		} //ending of loop
-		sc.close();
 	 } //ending of main
 } //ending of class MainEvaluation
