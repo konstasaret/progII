e3a5d5497f36e61d52ba811554c91d28b0883e1f
vickyviolintzi <t8190013@aueb.gr>
2020-12-22 10:47:36 +0200

    Η κλάση Fanswers τρέχει σωστά και έχει προσθεθεί η επιλογή "Έξοδος".

--- src/main/java/gr/aueb/dmst/StopSpread/Fanswers.java
+++ src/main/java/gr/aueb/dmst/StopSpread/Fanswers.java
@@ -1,76 +1,111 @@
 package gr.aueb.dmst.StopSpread;
 
 public class Fanswers {
-	public static void fAQ() {
-
-		int choice;
+	// public static void fAQ() {
+	public static void main(String[] args) {
 		Menus menouQ = new Menus();
 		Answers eisagwgi = new Answers();
-		menouQ.menu88();
-		System.out.println("Πατήστε 0 αν θέλετε να διαβάσετε γενικές πληροφορίες της έρευνας.");
-		System.out.println("Διαφορετικά πατήστε τον αριθμό της ερώτησης που θέλετε από 1 έως και 15.");
-		System.out.println("Παρακαλώ εισάγετε την επιλογή σας:");
-		choice = Inputs.rangeInt(0, 15);
-		boolean flag = true;
 
-		while (flag) {// begging of loop
-			switch (choice) {// beggining of switch
-			case 0:
-				eisagwgi.Eisagwgi();
-				break;
-			case 1:
-				eisagwgi.Answer1();
-				break;
-			case 2:
-				eisagwgi.Answer2();
-				break;
-			case 3:
-				eisagwgi.Answer3();
-				break;
-			case 4:
-				eisagwgi.Answer4();
-				break;
-			case 5:
-				eisagwgi.Answer5();
-				break;
-			case 6:
-				eisagwgi.Answer6();
-				break;
-			case 7:
-				eisagwgi.Answer7();
-				break;
-			case 8:
-				eisagwgi.Answer8();
-				break;
-			case 9:
-				eisagwgi.Answer9();
-				break;
-			case 10:
-				eisagwgi.Answer10();
-				break;
-			case 11:
-				eisagwgi.Answer11();
-				break;
-			case 12:
-				eisagwgi.Answer12();
-				break;
-			case 13:
-				eisagwgi.Answer13();
-				break;
-			case 14:
-				eisagwgi.Answer14();
-				break;
-			case 15:
-				eisagwgi.Answer15();
-				break;
-			case 16:
-				flag = false;
-				continue;
-			}// ending of switch
-				// na ftiajo ti sinexeia stop sinexizo
-				// ejereseis
-				// -
+		boolean loop = true;
 
-		} // ending of loop
+		do {
+			menouQ.menu88();
+			menouQ.fAQMenu1();
+			int choice = Inputs.rangeInt(0, 17);
+			boolean flag = true;
+			while (flag) {// begging of loop
+				switch (choice) {// beggining of switch
+				case 1:
+					eisagwgi.Eisagwgi();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 2:
+					eisagwgi.Answer1();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 3:
+					eisagwgi.Answer2();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 4:
+					eisagwgi.Answer3();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 5:
+					eisagwgi.Answer4();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 6:
+					eisagwgi.Answer5();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 7:
+					eisagwgi.Answer6();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 8:
+					eisagwgi.Answer7();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 9:
+					eisagwgi.Answer8();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 10:
+					eisagwgi.Answer9();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 11:
+					eisagwgi.Answer10();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 12:
+					eisagwgi.Answer11();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 13:
+					eisagwgi.Answer12();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 14:
+					eisagwgi.Answer13();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 15:
+					eisagwgi.Answer14();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 16:
+					eisagwgi.Answer15();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 17:
+					eisagwgi.Answer16();
+					menouQ.menu88();
+					choice = Inputs.rangeInt(0, 17);
+					break;
+				case 0:
+					flag = false;
+					break;
+				}
+				loop = false;
+			} // ending of loop
+		} while (loop);
 	}
 }
