57c0dcf1a8e54ac253b1145e95dde1383a8ca955
vickyviolintzi <t8190013@aueb.gr>
2020-12-28 12:40:52 +0200

    Changed method rangeInt to non static. DO NOT CHANGE IT AGAIN!

--- src/main/java/gr/aueb/dmst/StopSpread/Inputs.java
+++ src/main/java/gr/aueb/dmst/StopSpread/Inputs.java
@@ -7,30 +7,34 @@ import java.util.Scanner;
  * @author alexd
  */
 public class Inputs {
- /**Handles user input errors and returns an integer in the range of
-  *  lower_boundary and upper_boundary including the limits
-  * @param lower_boundary
-  * @param upper_boundary
-  * @return An Integer as : lower_boundary <= Integer <= upper_boundary*/
-  public static int rangeInt(int lower_boundary, int upper_boundary) {
-    Scanner in = new Scanner(System.in);
-    int inp = -1;
-    try {
-      inp = in.nextInt();
-      in.nextLine();
-      if (inp < lower_boundary || inp > upper_boundary) {
-        throw new InputMismatchException();
-      }
-    } catch (InputMismatchException e) {
-      System.err.println("Παρακαλώ εισάγετε αριθμό απο το " + lower_boundary + " μεχρι το " + upper_boundary);
-      inp = rangeInt(lower_boundary, upper_boundary);
-    }
-    return inp;
-  }
-  /** @return user String input*/
+	/**
+	 * Handles user input errors and returns an integer in the range of
+	 * lower_boundary and upper_boundary including the limits
+	 * 
+	 * @param lower_boundary
+	 * @param upper_boundary
+	 * @return An Integer as : lower_boundary <= Integer <= upper_boundary
+	 */
+	public int rangeInt(int lower_boundary, int upper_boundary) {
+		Scanner in = new Scanner(System.in);
+		int inp = -1;
+		try {
+			inp = in.nextInt();
+			in.nextLine();
+			if (inp < lower_boundary || inp > upper_boundary) {
+				throw new InputMismatchException();
+			}
+		} catch (InputMismatchException e) {
+			System.err.println("Παρακαλώ εισάγετε αριθμό απο το " + lower_boundary + " μεχρι το " + upper_boundary);
+			inp = rangeInt(lower_boundary, upper_boundary);
+		}
+		return inp;
+	}
 
-  public String stringScanner() {
-    Scanner in = new Scanner(System.in);
-    return in.nextLine();
-  }
+	/** @return user String input */
+
+	public String stringScanner() {
+		Scanner in = new Scanner(System.in);
+		return in.nextLine();
+	}
 }
