731262fd842eaeafac2d74087e065c216bdcca19
= <t8190130@aueb.gr>
2020-12-29 21:16:55 +0200

    gsx anadiamorfsi

--- src/main/java/gr/aueb/dmst/StopSpread/GSX.java
+++ src/main/java/gr/aueb/dmst/StopSpread/GSX.java
@@ -18,66 +18,30 @@ public class GSX {
 	public void gsxToTCP() {
 		Inputs inp = new Inputs();
 		boolean contloop = true;
-		boolean contloopin = true;
 		Menus menu = new Menus();
 		do {
 			System.out.println("Διαθέσιμες επιλογές:");
 			menu.greekStatsMenu();
 			int choice = inp.rangeInt(1, 4);
-			boolean flag2;
 			int option;
 			while (choice != 4) { // beginning of loop
 				if (choice == 1) {
-					showSite("https://covid19.gov.gr/covid19-live-analytics/");
+					String url = decideURL(choice, 0);
+					showSite(url);
 				} else if (choice == 2) {
-					do {
+					while (true) {
 						menu.greekStatsMenu2();
 						option = inp.rangeInt(1, 11);
-						flag2 = true;
-						while (flag2 == true) {
-							switch (option) { // beginning of switch
-							case 1:
-								showSite("https://covid19greece.com/geografiko-diamerisma/attiki");
-								break;
-							case 2:
-								showSite("https://covid19greece.com/geografiko-diamerisma/ipeiros");
-								break;
-							case 3:
-								showSite("https://covid19greece.com/geografiko-diamerisma/thessalia");
-								break;
-							case 4:
-								showSite("https://covid19greece.com/geografiko-diamerisma/thraki");
-								break;
-							case 5:
-								showSite("https://covid19greece.com/geografiko-diamerisma/kriti");
-								break;
-							case 6:
-								showSite("https://covid19greece.com/geografiko-diamerisma/makedonia");
-								break;
-							case 7:
-								showSite("https://covid19greece.com/geografiko-diamerisma/nisia-aigaioy-pelagoys");
-								break;
-							case 8:
-								showSite("https://covid19greece.com/geografiko-diamerisma/nisia-ionioy-pelagoys");
-								break;
-							case 9:
-								showSite("https://covid19greece.com/geografiko-diamerisma/peloponnisos");
-								break;
-							case 10:
-								showSite("https://covid19greece.com/geografiko-diamerisma/sterea-ellada");
-								break;
-							case 11:
-								flag2 = false;
-								continue;
-							} // end of switch
-							menu.greekStatsMenu2();
-							option = inp.rangeInt(1, 11);
-							contloopin = false;
-						} // end of loop
-
-					} while (contloopin);
+						
+						String url = decideURL(choice, option);
+						if(!url.equals("exit"))
+							showSite(url);
+						else
+							break;
+					} // end of loop
 				} else if (choice == 3) {
-					showSite("https://betmasters.gr/koronoios/50170-statistika-stin-ellada-prognostika/");
+					String url = decideURL(choice, 0);
+					showSite(url);
 				}
 
 				menu.greekStatsMenu();
@@ -85,6 +49,52 @@ public class GSX {
 			}
 			contloop = false;
 		} while (contloop);
+	}
+	
+	public String decideURL(int choice, int NomosOption) {
+		String url = "";
+		
+		if(choice == 1)
+			url = "https://covid19.gov.gr/covid19-live-analytics/";
+		else if(choice == 3)
+			url = "https://betmasters.gr/koronoios/50170-statistika-stin-ellada-prognostika/";
+		else if(choice == 2) {
+			switch (NomosOption) { // beginning of switch
+			case 1: 
+				url = "https://covid19greece.com/geografiko-diamerisma/attiki";
+				break;
+			case 2:
+				url = "https://covid19greece.com/geografiko-diamerisma/ipeiros";
+				break;
+			case 3:
+				url = "https://covid19greece.com/geografiko-diamerisma/thessalia";
+				break;
+			case 4:
+				url = "https://covid19greece.com/geografiko-diamerisma/thraki";
+				break;
+			case 5:
+				url = "https://covid19greece.com/geografiko-diamerisma/kriti";
+				break;
+			case 6:
+				url = "https://covid19greece.com/geografiko-diamerisma/makedonia";
+				break;
+			case 7:
+				url = "https://covid19greece.com/geografiko-diamerisma/nisia-aigaioy-pelagoys";
+				break;
+			case 8:
+				url = "https://covid19greece.com/geografiko-diamerisma/nisia-ionioy-pelagoys";
+				break;
+			case 9:
+				url = "https://covid19greece.com/geografiko-diamerisma/peloponnisos";
+				break;
+			case 10:
+				url = "https://covid19greece.com/geografiko-diamerisma/sterea-ellada";
+				break;
+			case 11:
+				url = "exit";
+			}
+		}
+		return url;
 	}
 
 	// small change to see if it's working
