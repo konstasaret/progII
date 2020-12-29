36ae2629275a2891c92f5ca4826a943bb406b40a
= <t8190130@aueb.gr>
2020-12-29 21:18:38 +0200

    2 tests with junit GSX , Inputs

--- src/main/java/gr/aueb/dmst/StopSpread/TestGSX.java
+++ src/main/java/gr/aueb/dmst/StopSpread/TestGSX.java
@@ -1,13 +1,40 @@
 package gr.aueb.dmst.StopSpread;
 
 import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestGSX {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	GSX obj;
+	
+	@Before
+	public void setUp() {
+		System.out.println("Starting testing of GSX.jaxa");
+		obj = new GSX();
 	}
-
+	
+	@Test
+	public void testURLDecides() {
+		assertEquals("https://covid19.gov.gr/covid19-live-analytics/",  obj.decideURL(1, 0));
+		assertEquals("https://betmasters.gr/koronoios/50170-statistika-stin-ellada-prognostika/",  obj.decideURL(3, 0));
+		assertEquals("https://covid19greece.com/geografiko-diamerisma/attiki",  obj.decideURL(2, 1));
+		assertEquals("https://covid19greece.com/geografiko-diamerisma/ipeiros",  obj.decideURL(2, 2));
+		assertEquals("https://covid19greece.com/geografiko-diamerisma/thessalia",  obj.decideURL(2, 3));
+		assertEquals("https://covid19greece.com/geografiko-diamerisma/thraki",  obj.decideURL(2, 4));
+		assertEquals("https://covid19greece.com/geografiko-diamerisma/kriti",  obj.decideURL(2, 5));
+		assertEquals("https://covid19greece.com/geografiko-diamerisma/makedonia",  obj.decideURL(2, 6));
+		assertEquals( "https://covid19greece.com/geografiko-diamerisma/nisia-aigaioy-pelagoys",  obj.decideURL(2, 7));
+		assertEquals("https://covid19greece.com/geografiko-diamerisma/nisia-ionioy-pelagoys",  obj.decideURL(2, 8));
+		assertEquals("https://covid19greece.com/geografiko-diamerisma/peloponnisos",  obj.decideURL(2, 9));
+		assertEquals("https://covid19greece.com/geografiko-diamerisma/sterea-ellada",  obj.decideURL(2, 10));
+		assertEquals("exit",  obj.decideURL(2, 11));
+		
+	}
+	
+	
+	@After
+	public void finish() {
+		System.out.println("Finished testing of GSX.jaxa");
+	}
 }
--- src/main/java/gr/aueb/dmst/StopSpread/TestInputs.java
+++ src/main/java/gr/aueb/dmst/StopSpread/TestInputs.java
@@ -1,44 +1,44 @@
 package gr.aueb.dmst.StopSpread;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestInputs {
+	Inputs obj;
 
+	@Before
+	public void setUp() {
+		System.out.println("Starting testing of Inputs.java");
+		obj = new Inputs();
+	}
+	
+	@After
+	public void finish() {
+		System.out.println("Finished testing of Inputs.java");
+	}
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void isNumWithinBounds() {
+		//periptoseis errors
+		assertTrue(obj.isNumberAllowed(1, 10, 5)); // Mesa sta bounds
+		assertFalse(obj.isNumberAllowed(1, 10, 0)); // Eksw apo aristera
+		assertFalse(obj.isNumberAllowed(1, 10, 11)); // Eksw apo deksia
+//		x: input
+		
+		//lower < x < upper
+		//lower < upper < x tf
+		//x < lower < upper ft
+		//upper < x < lower ff
 	}
 
-	@Test
-	public void isNumWithinBounds() throws IOException {
-		// Inputs check = new Inputs();
-		assertEquals(5, Inputs.rangeInt(1, 10));
-	}
-
-	@Test
-	public void testIntWithinBounds() throws IOException {
-		// Inputs input = new Inputs();
-
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter a number");
-		int lowNum = sc.nextInt();
-		int uppNum = sc.nextInt();
-		assertEquals(5, Inputs.rangeInt(lowNum, uppNum));
-		sc.close();
-	}
-
-	/*
-	 * public String mes = "yes";
-	 *
-	 * @Test(expected = ArithmeticException.class) public void testExceptions() {
-	 * System.out.println("JUnit message prints:" + mes);
-	 *
-	 * }
-	 */
 }
