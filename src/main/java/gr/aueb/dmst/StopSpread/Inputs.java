45c77cd6fdcc758a441c3d4c57bb1bf3d8abcd7a
AirlineDog <t8190042@aueb.gr>
2020-12-30 11:50:11 +0200

    return inputs to it's original state

--- src/main/java/gr/aueb/dmst/StopSpread/Inputs.java
+++ src/main/java/gr/aueb/dmst/StopSpread/Inputs.java
@@ -12,37 +12,33 @@ public class Inputs {
 	 * Handles user input errors and returns an integer in the range of
 	 * lower_boundary and upper_boundary including the limits
 	 *
-	 * @param lower_boundary
-	 * @param upper_boundary
+	 * @param lower_boundary : the lowest number possible
+	 * @param upper_boundary : the highest number possible
 	 * @return An Integer as : lower_boundary <= Integer <= upper_boundary
 	 */
-	
 	public int rangeInt(int lower_boundary, int upper_boundary) {
-		Scanner in = new Scanner(System.in);
-		int inp = -1;
-		inp = in.nextInt();
-		in.nextLine();
-		if (!isNumberAllowed(lower_boundary, upper_boundary, inp)) {
+		Scanner in = new Scanner(System.in);// Create a scanner
+		int inp = -1;// Initialize input
+		try {
+			inp = in.nextInt();// Get an Integer
+			in.nextLine();// Consume new line character
+
+			// number out of the desired range
+			if (inp < lower_boundary || inp > upper_boundary) {
+				throw new InputMismatchException();
+			}
+
+		} catch (InputMismatchException e) {
 			System.err.println("Παρακαλώ εισάγετε αριθμό απο το " + lower_boundary + " μεχρι το " + upper_boundary);
 			inp = rangeInt(lower_boundary, upper_boundary);
 		}
 		return inp;
 	}
-	
-	public boolean isNumberAllowed(int lower_boundary, int upper_boundary, int givenInput) {
-		return (givenInput >= lower_boundary && givenInput <= upper_boundary);
-		
-		
-	
-		
-		
-		
-	}
 
 	/** @return user String input */
-
 	public String stringScanner() {
-		Scanner in = new Scanner(System.in);
-		return in.nextLine();
+		Scanner in = new Scanner(System.in);// Create new scanner
+		return in.nextLine();// Get a String
 	}
+
 }
