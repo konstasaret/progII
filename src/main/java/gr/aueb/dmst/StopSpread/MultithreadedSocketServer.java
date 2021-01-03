f6ebbdaf6e78bbb95ee2067db25aaa16045ec249
Chrysoula7 <xapriv@gmail.com>
2021-01-03 12:47:33 +0200

    changed format Multithread with checkstyle

--- src/main/java/gr/aueb/dmst/StopSpread/MultithreadedSocketServer.java
+++ src/main/java/gr/aueb/dmst/StopSpread/MultithreadedSocketServer.java
@@ -9,31 +9,30 @@ import java.net.Socket;
  *
  */
 public class MultithreadedSocketServer {
-
-	/**
-	 * Starts Server
-	 *
-	 * @param args
-	 * @throws IOException
-	 */
-	public static void main(String[] args) {
-		try {
-			ServerSocket server = new ServerSocket(8888);
-
-			int counter = 0;
-			System.out.println("Server Started ....");
-			while (true) {
-				counter++;
-				Socket serverClient = server.accept(); // server accept the client connection request
-				System.out.println(">> " + "Client No:" + counter + " started!");
-				ServerClientThread sct = new ServerClientThread(serverClient, counter); // send the request to a
-																						// separate
-																						// thread
-				sct.start();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+  /**
+   * Starts Server
+   * 
+   * @param args
+   * @throws IOException
+   */
+  
+  public static void main(String[] args) {
+    try {
+	    ServerSocket server = new ServerSocket(8888);
+	    int counter = 0;
+	    System.out.println("Server Started ....");
+	    while (true) {
+	        counter++;
+            Socket serverClient = server.accept(); // server accept the client connection request
+	        System.out.println(">> " + "Client No:" + counter + " started!");
+            ServerClientThread sct = new ServerClientThread(serverClient, counter); // send the request to a
+                                                                                    // separate
+                                                                                    // thread
+	        sct.start();
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+
+  }
 }
